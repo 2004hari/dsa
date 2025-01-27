328e57255984e42bf57641a8cbeb553af94460b5
2004hari <bhariharan100@gmail.com>
2025-01-28 04:26:37 +0530

    give the  change in  print statement

--- find the biggest number.py
+++ find the biggest number.py
@@ -1,9 +1,9 @@
 def findingthebignumber(n1, n2):
     # using the if else statement to find the biggest number of two  elements
     if n1 > n2:
-        print("n1 is yhe biggest number")
+        print("The biggest number is" , n1)
     elif n2 > n1:
-        print("n2 is the biggest number")
+        print("The biggest number" , n2)
     else:
         print("Both numbers are equal")
    # giving the exeception handling   
