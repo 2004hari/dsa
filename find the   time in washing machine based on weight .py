b38b0f80a8f4ef5e754d7c53c8b119ee200779dc
2004hari <bhariharan100@gmail.com>
2025-01-29 04:07:07 +0530

    giving the command

--- find the   time in washing machine based on weight .py
+++ find the   time in washing machine based on weight .py
@@ -1,12 +1,14 @@
 def washing_machine(weight  ):
+    #using the conditional statements  if elif 
     
     if weight <0 :
         return ("give positive number ")
     elif weight == 0 :
-        return("Washing machine will take 0 seconds ")
+        return("Washing machine will take 0 second")
       
     elif weight >7000:
-        return("over weight ")      
+        return("over weight ")  
+    # setting the condition   water level  if is ture is send tonparticular condition and return yhr output     
       
     
     if waterlevel =="low" :
@@ -19,10 +21,14 @@ def washing_machine(weight  ):
     elif waterlevel  == "large" :
         if 4000 < weight <6000:
             return ("Washing machine will take 45 min ")  
-    return (" invalid weight ")    
+       #suppose all condition are wrong it return invald weight 
+    return (" invalid weight ") 
+# giving the value    
  
 weight = 2002
 waterlevel = "low"
+# calling the funtion 
+
 
 result = washing_machine(weight)     
 print(result)
