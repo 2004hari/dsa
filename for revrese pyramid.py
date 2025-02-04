88c954572bdd5934da1221fcf9a3ec876512fa0b
2004hari <bhariharan100@gmail.com>
2025-02-04 21:37:08 +0530

    for reverse pyramid

--- for revrese pyramid.py
+++ for revrese pyramid.py
@@ -1,7 +1,8 @@
 n= int(input("enter the number of row"))
-for i in range(0,n):
-    for j in range(0,n+i+1):
-        print(end=" ")
-    for k in range(0,i-1):
-        print("*",end=" ")
-    print()        
+for i in range(n,0,-1):
+    for j in range(0,n-i):
+        print(" ",)
+    for k in range(0,i):
+        print("*",end="")
+print()            
+    
