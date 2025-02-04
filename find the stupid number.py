c910adbb5fec46503b805b752951179e153a2a61
2004hari <bhariharan100@gmail.com>
2025-02-04 11:36:41 +0530

    find the all poosible password in the list  using the nested loop in the program

---
+++ find the stupid number.py
@@ -0,0 +1,15 @@
+def stupid_number(n,l):
+    letter= "abcdefghijklmnopqrstuvwxyz"
+    passwords =  []
+    for  char1 in range (1,n+1):
+        for char2 in range (1,n+1):
+            for char3 in range (l):
+                for  char4 in range (l):
+                    for char5 in range (1,n+1):
+                        if char5 > char1 and char5 > char2:
+                            password= str(char1) +str(char2) +str(char3) +str(char4) +str(char5)
+                            passwords. append(password)
+                            print (" ".join(passwords))                            
+l = int(input(" entert the number "))                            
+n= int ( input ("entert the number"))
+stupid_number(n,l)
