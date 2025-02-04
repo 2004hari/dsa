0371680b6d1d590a5c8ea6868a1051ec1e420786
2004hari <bhariharan100@gmail.com>
2025-02-04 10:57:33 +0530

    change in code

--- fizz bizz .py
+++ fizz bizz .py
@@ -3,13 +3,13 @@ def fizzBuzz(n):
     
     for i in range(1, n + 1):  
         if i % 3 == 0 and i % 5 == 0: 
-            # Divisible by both 3 and 5
+            # Divide by both 3 and 5
             result.append("FizzBuzz")
         elif i % 3 == 0:  
-            # Divisible by 3
+            # Divide by 3
             result.append("Fizz")
         elif i % 5 == 0:  
-            # Divisible by 5
+            # Divide by 5
             result.append("Buzz")
         else: 
             # Not divisible
